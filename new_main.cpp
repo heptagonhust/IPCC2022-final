cd4dc3d7f828dba5123a757152345f4f43364fe7
Shawlley <wsyy0619@gmail.com>
2022-10-24 20:21:37 +0800

    remove parallel sort, enable tbb on hust02

--- new_main.cpp
+++ new_main.cpp
@@ -100,7 +100,8 @@ struct UnionFindSet {
 void kruskal(int node_cnt, vector<Edge> &edges, vector<Edge> &tree_edges,
              vector<Edge> &off_tree_edges) {
   ScopeTimer t_("kruskal");
-  stable_sort(std::execution::par_unseq, edges.begin(), edges.end());
+  // stable_sort(std::execution::par_unseq, edges.begin(), edges.end());
+  sort(edges.begin(), edges.end());
   tree_edges.reserve(node_cnt - 1);
   off_tree_edges.reserve(edges.size() - (node_cnt - 1));
   UnionFindSet ufs(node_cnt + 1);
@@ -191,7 +192,8 @@ void sort_off_tree_edges(vector<Edge> &edges, const vector<double> &depth) {
     printf("%d %d %lf %lf\n", x.a, x.b, x.weight, x.origin_weight);
   }
 #endif
-  stable_sort(std::execution::par_unseq, edges.begin(), edges.end());
+  // stable_sort(std::execution::par_unseq, edges.begin(), edges.end());
+  sort(edges.begin(), edges.end());
 }
 
 void mark_ban_edges(vector<bool> &ban, const vector<int> &ban_edges) {
