5886c493d3f69c1c9800ca69a5356a13f1db3cc0
mgt <mgt@oi-wiki.org>
2022-11-29 23:08:24 +0800

    move data

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [ -d ./build ] ||  mkdir build
-g++ -o build/ng-sparsify new_main.cpp  -std=c++17 -lpthread  -lboost_system -Wall -Wno-unused-result -Wno-sign-compare -Ofast -g -mavx2 -ltbb -flto
+g++ -o build/ng-sparsify new_main.cpp -std=c++17 -lpthread -Wall -Wno-unused-result -Wno-sign-compare -Ofast -g -mavx2 -ltbb -flto
