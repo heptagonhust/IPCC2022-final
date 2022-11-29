7594a903136f6c564fa8010bfd49ba9022991cc2
Shawlley <wsyy0619@gmail.com>
2022-11-29 23:41:53 +0800

    README

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 [ -d ./build ] ||  mkdir build
-g++ -o build/ng-sparsify new_main.cpp -std=c++17 -lpthread -Wall -Wno-unused-result -Wno-sign-compare -Ofast -g -mavx2 -ltbb -flto
+g++ -o build/sparsify new_main.cpp -std=c++17 -lpthread -Wall -Wno-unused-result -Wno-sign-compare -Ofast -g -mavx2 -ltbb -flto
